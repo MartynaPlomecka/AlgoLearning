fce319eea55d34273f07eeba04888a94985c5cfa
martynaplomecka <martyna.plomecka@uzh.ch>
2022-03-09 00:16:36 +0100

    RC

---
+++ rotationalCipher.py
@@ -0,0 +1,49 @@
+def rotationalCipher(s, factor):
+    out = ''
+    for c in s:
+        if c.isnumeric():
+            out += str((int(c) + factor) % 10)
+        elif c.isupper():
+            temp = (ord(c)-ord('A') + factor) % 26
+            out += chr(ord('A') + temp)
+        elif c.islower():
+            temp = (ord(c)-ord('a') + factor) % 26
+            out += chr(ord('a') + temp)
+        else:
+            out += c
+    return out
+
+def printString(string):
+  print('[\"', string, '\"]', sep='', end='')
+
+test_case_number = 1
+
+def check(expected, output):
+  global test_case_number
+  result = False
+  if expected == output:
+    result = True
+  rightTick = '\u2713'
+  wrongTick = '\u2717'
+  if result:
+    print(rightTick, 'Test #', test_case_number, sep='')
+  else:
+    print(wrongTick, 'Test #', test_case_number, ': Expected ', sep='', end='')
+    printString(expected)
+    print(' Your output: ', end='')
+    printString(output)
+    print()
+  test_case_number += 1
+
+if __name__ == "__main__":
+  input_1 = "All-convoYs-9-be:Alert1."
+  rotation_factor_1 = 4
+  expected_1 = "Epp-gsrzsCw-3-fi:Epivx5."
+  output_1 = rotationalCipher(input_1, rotation_factor_1)
+  check(expected_1, output_1)
+
+  input_2 = "abcdZXYzxy-999.@"
+  rotation_factor_2 = 200
+  expected_2 = "stuvRPQrpq-999.@"
+  output_2 = rotationalCipher(input_2, rotation_factor_2)
+  check(expected_2, output_2)
